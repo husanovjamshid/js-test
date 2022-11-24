7fc20e01202ceab522d02d53f8c2f59f930432ed
husanovjamshid <husanovjamshid11@gmail.com>
2022-11-24 22:42:53 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+var name = prompt('Ismingizni kiriting :');
+
+var question1 = prompt("CSS da style ni necha xil ulash mumkin? \n A) 1  */ \n B) 2 \n C) 3 \n D) 4");
+var question1 = prompt("Saytda nechta h1 tagini ishlatish mumkin?\n A) 3ta \n B) Bir qacha \n C) 2ta \n D) 1ta");
+var question1 = prompt("HTML qanday til?\n A) Dasturlash tili \n B) Dasturlash tili emas \n C) Belgilash tili \n D) To'g'ri javob yo'q");
+var question1 = prompt("Align-items: center; vazifasi?\n A) X o'qi bo'yicha o'rtaga olib keladi \n B) B o'qi bo'yicha o'rtaga olib keladi \n C) X va Y o'qi bo'yicha o'rtaga olib keladi ");
+
+
+console.log("1-Savol" + '\n' + 'Sizning javobingiz :' + question1 + ' ' + "To'g'ri javob:C")
+console.log("2-Savol" + '\n' + 'Sizning javobingiz :' + question1 + ' ' + "To'g'ri javob:D")
+console.log("3-Savol" + '\n' + 'Sizning javobingiz :' + question1 + ' ' + "To'g'ri javob:C")
+console.log("4-Savol" + '\n' + 'Sizning javobingiz :' + question1 + ' ' + "To'g'ri javob:B")
